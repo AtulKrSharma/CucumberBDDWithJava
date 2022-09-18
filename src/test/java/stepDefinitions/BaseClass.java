876e9822dbe443cf968aef29b4d1f33e09413e25
Sharma <AtulKumar.Sharma@bondbrandloyalty.com>
2022-09-18 15:00:31 -0400

    refactored code

--- src/test/java/stepDefinitions/BaseClass.java
+++ src/test/java/stepDefinitions/BaseClass.java
@@ -33,37 +33,4 @@ public class BaseClass {
 		return (generatedString1);
 	}
 
-	public void setup() throws IOException {
-
-		// Logger steps
-		logger = Logger.getLogger("nopcommerce");
-		PropertyConfigurator.configure("Log4j.properties");
-		logger.setLevel(Level.DEBUG);
-		// Logger steps done
-
-		// Loading Config.properties file steps
-		configProp = new Properties();
-		FileInputStream configPropfile = new FileInputStream("config.properties");
-		configProp.load(configPropfile);
-		// Loading Config.properties file is done
-
-		String br = configProp.getProperty("browser");
-
-		if (br.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", configProp.getProperty("firefoxpath"));
-			driver = new FirefoxDriver();
-		}
-
-		else if (br.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", configProp.getProperty("chromepath"));
-			driver = new ChromeDriver();
-		}
-
-		else if (br.equals("ie")) {
-			System.setProperty("webdriver.ie.driver", configProp.getProperty("iepath"));
-			driver = new InternetExplorerDriver();
-		}
-
-	}
-
 }
